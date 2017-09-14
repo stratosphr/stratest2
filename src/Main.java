8c3406281799abdf1e5416ba4a6f3f4c3f522c2f
stratosphr <stratosphr@gmail.com>
2017-09-14 11:28:44 +0200

    Z3 libraries

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import com.microsoft.z3.Context;
+
 public class Main {
 
     public static void main(String[] args) {
+        Context context = new Context();
+        System.out.println(context.mkInt(2));
     }
 
 }
